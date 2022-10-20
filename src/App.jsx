e411d9324441656a10bba77071aaa909cc445740
Eva S <evamarie.songco@gmail.com>
2022-10-20 15:42:14 -0700

    Added api function accessing external data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,58 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
-import beers from './data/beers';
+// import beers from './data/beers';
 import Main from './containers/Main/Main';
 import NavBar from './containers/NavBar/NavBar';
 
-
 const App = () => {
-  // const beersArr = beers
-  //   .filter((beer) => beer.name);
-
+// ===========================================================
+  
   const filtersList = [
     {id: 1, text: "High ABV > 6%", checked: false, field: "abv", operator: ">", value: 6 },
     {id: 2, text: "Classic Range (< 2010)", checked: false, field: "first_brewed", operator: "<", value: 2010 },
     {id: 3, text: "Acidic (ph < 4)", checked: false, field: "ph", operator: "<", value: 4 },
   ];
 
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState([]);
   const [filterArr, updateFilterArr] = useState(filtersList);
 
-  const beersArr = beers.filter((beer) => {
+  // ===========================================================
+  // API CODE
+
+  const url = "https://api.punkapi.com/v2/beers";
+  const [beerArr,SetBeerArr] = useState([]);
+  const [pageCtr,SetPageCtr] = useState(1);
+  const [perPageCtr,SetperPageCtr] = useState(80);
+
+  // useEffect(() => {
+  //   getBeers();
+  //   },[]
+  // );
+
+  // const getBeers = async () => {
+  //   const res = await fetch (url);
+  //   const data = await res.json();
+  //   const beers = data.filter((beer) => beer.name);
+  //   SetBeerArr(beers);
+  //   console.log (beers);
+  //   console.log (beerArr);
+  //  }
+
+  useEffect(() => {
+    getBeersByPage();
+    },[]
+  );
+
+   const getBeersByPage = async () => {
+    let res = await fetch (`${url}?page=${pageCtr}&per_page=${perPageCtr}`);
+    const data = await res.json();
+    const beers = data.filter((beer) => beer.name);
+    SetBeerArr(beers);
+    console.log (beers);
+    console.log (beerArr);
+   }
+
+   const beersArr = beerArr.filter((beer) => {
     const beersNameLower = beer.name.toLowerCase();
 
     return beersNameLower.includes(searchTerm)
@@ -27,6 +61,16 @@ const App = () => {
          && (filterArr[2].checked ? beer.ph < 4: true )
   });
 
+// ===========================================================
+  
+  // const beersArr = beers.filter((beer) => {
+  //   const beersNameLower = beer.name.toLowerCase();
+
+  //   return beersNameLower.includes(searchTerm)
+  //        && (filterArr[0].checked ? beer.abv > 6: true )
+  //        && (filterArr[1].checked ? beer.first_brewed.substr(3,6) < 2010: true )
+  //        && (filterArr[2].checked ? beer.ph < 4: true )
+  // });
 
   const toggleCheckedFilter = (filterId) => {
     const updatedFilterArr = filterArr.map(filter => 
@@ -40,11 +84,10 @@ const App = () => {
     setSearchTerm(cleanInput);
   };
 
-
+  // ===========================================================
   
   return (
     <>
-        
       <h1 className="app-title">Beer Catalogue</h1>
       <div className="app-container">      
         <nav className="navbar">
@@ -53,7 +96,6 @@ const App = () => {
           handleInput={handleInput} 
           filterArr={filterArr} 
           toggleCheckedFilter={toggleCheckedFilter}/>
-          {/* <NavBar beersArr={beersArr}/> */}
         </nav>
         <main className="main">
           <div className="card-container">
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -12,7 +12,7 @@ const Main = (props) => {
     <section className="cards-list">
       <h2 className="cards-list__heading">Explore Catalog</h2>
       <div className="all-beers">
-        <BeerCards beersArr={beersArr.slice(0,beersArr.length)}/>
+        <BeerCards title="Explore" beersArr={beersArr.slice(0,beersArr.length)}/>
       </div>
     </section>
     </>    
