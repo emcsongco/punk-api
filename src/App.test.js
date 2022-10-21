0c987f4da7b49035b2882bb9e929750d81dab443
Eva S <evamarie.songco@gmail.com>
2022-10-21 15:00:53 -0700

    Added more responsive styling to beer cards

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { getByAltText, render, screen } from "@testing-library/react";  //test that form is rendered correctly on screen
+import userEvent from "@testing-library/user-event";
+
 import App from './App';
 
-test('renders learn react link', () => {
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it('should render the Beer Catalogue', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/beer catalogue/i);
   expect(linkElement).toBeInTheDocument();
 });
